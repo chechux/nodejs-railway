29e55ca363c6821236e5a983f84be30822ccdd9d
Jesús Nieto <jnietoribera@gmail.com>
2023-01-25 13:23:27 +0100

    añadimos para crear usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pool = require("../db.js")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const [rows] = await pool.query("SELECT * FROM users")
+  res.json(rows)
 });
+
+router.get('/create', async function(req, res, next) {
+  const result = await pool.query("SELECT 'hello word' AS RESULT")
+  console.log(result)
+  res.json(result[0])
+});
+
+router.get('/ping', async function(req, res, next) {
+  const result = await pool.query("INSERT INTO users(name) VALUES('KITTY')")
+  console.log(result)
+  res.json(result)
+});
+
 
 module.exports = router;
